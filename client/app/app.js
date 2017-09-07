73f050340ffbee33e8707bbfdc93d5ed394b564c
zvizvi <5633910@gmail.com>
2017-09-07 18:56:54 +0300

    add srvicesModule

--- client/app/app.js
+++ client/app/app.js
@@ -4,6 +4,7 @@ import appConfig from './app.config';
 import Common from './common/common';
 import Components from './components/components';
 import Directives from './directives/directives';
+import Services from './services/services';
 import AppComponent from './app.component';
 import 'normalize.css';
 
@@ -11,7 +12,8 @@ angular.module('app', [
   uiRouter,
   Common,
   Components,
-  Directives
+  Directives,
+  Services
 ])
   .config(appConfig)
   .component('app', AppComponent);
